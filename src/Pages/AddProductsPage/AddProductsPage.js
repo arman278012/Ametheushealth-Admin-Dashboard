c464d2a2f08b1fe2ff511e791ad2ce5d7f7b4dc4
arman278012 <aa278012@gmail.com>
2024-07-11 18:50:16 +0530

    11 july

--- src/Pages/AddProductsPage/AddProductsPage.js
+++ src/Pages/AddProductsPage/AddProductsPage.js
@@ -1,17 +1,29 @@
 import React from "react";
 import MySideBar from "../../Components/MySideBar/MySideBar";
 import Addproduct from "../../Components/AddProduct/Addproduct";
+import MobileNavbar from "../../Components/MobileNavbar/MobileNavbar";
 
 const AddProductsPage = () => {
   return (
-    <div className="flex">
-      <div>
-        <MySideBar />
+    <>
+      <div className="sm:flex hidden">
+        <div>
+          <MySideBar />
+        </div>
+        <div className="w-[100%]">
+          <Addproduct />
+        </div>
       </div>
-      <div className="w-[100%]">
-        <Addproduct />
+
+      <div className="sm:hidden block">
+        <div>
+          <MobileNavbar/>
+        </div>
+        <div className="w-[100%]">
+          <Addproduct />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/Pages/AllCategoriesPage/AllCategoriesPage.js
+++ src/Pages/AllCategoriesPage/AllCategoriesPage.js
@@ -1,18 +1,31 @@
 import React from "react";
 import MySideBar from "../../Components/MySideBar/MySideBar";
 import AllCategories from "../../Components/AllCategories/AllCategories";
+import MobileNavbar from "../../Components/MobileNavbar/MobileNavbar";
 
 const AllCategoriesPage = () => {
   return (
-    <div className="flex">
-      <div>
-        <MySideBar />
+    <>
+      <div className="sm:flex hidden">
+        <div>
+          <MySideBar />
+        </div>
+
+        <div className="w-[100%]">
+          <AllCategories />
+        </div>
       </div>
 
-      <div className="w-[100%]">
-        <AllCategories />
+      <div className="sm:hidden block">
+        <div>
+          <MobileNavbar />
+        </div>
+
+        <div className="w-[100%]">
+          <AllCategories />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
