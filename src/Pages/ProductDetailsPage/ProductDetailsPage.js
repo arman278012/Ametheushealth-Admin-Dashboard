028afc77e4d77279bd7ec88e95dbbd05f2c6436c
arman278012 <aa278012@gmail.com>
2024-07-12 19:11:01 +0530

    12 july

--- src/Pages/ProductDetailsPage/ProductDetailsPage.js
+++ src/Pages/ProductDetailsPage/ProductDetailsPage.js
@@ -1,17 +1,29 @@
 import React from "react";
 import MySideBar from "../../Components/MySideBar/MySideBar";
 import ProductDetails from "../../Components/ProductsDetails/ProductDetails";
+import MobileNavbar from "../../Components/MobileNavbar/MobileNavbar";
 
 const ProductDetailsPage = () => {
   return (
-    <div className="flex">
-      <div>
-        <MySideBar />
+    <>
+      <div className="sm:flex hidden">
+        <div>
+          <MySideBar />
+        </div>
+        <div className="w-[100%]">
+          <ProductDetails />
+        </div>
       </div>
-      <div className="w-[100%]">
-        <ProductDetails />
+
+      <div className="sm:hidden block">
+        <div>
+          <MobileNavbar />
+        </div>
+        <div className="w-[100%]">
+          <ProductDetails />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
