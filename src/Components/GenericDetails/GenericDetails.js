2c3a7b061d43e332d00f15a2ca3c90518a3c7566
arman278012 <aa278012@gmail.com>
2024-07-16 18:53:05 +0530

    generic details fetched

--- src/Components/GenericDetails/GenericDetails.js
+++ src/Components/GenericDetails/GenericDetails.js
@@ -38,12 +38,16 @@ const GenericDetails = () => {
         <div className="parent flex flex-col gap-10 px-10 py-5 bg-white">
           <div>
             <p className="font-bold uppercase">{genericData.name}</p>
-            
           </div>
 
           <div className="main-content w-[100%] flex flex-col gap-10">
             <div className="information w-[100%] bg-gray-100 shadow-md p-5">
-              <p className="  uppercase">{genericData.name} Uses</p>
+              <p className="  uppercase">
+                Uses of{" "}
+                <span className="text-red-500 font-semibold">
+                  {genericData.name}
+                </span>{" "}
+              </p>
               <div className="bg-gray-300 h-[1px] w-[270px] mt-1"></div>
 
               <div className="text-justify">
@@ -52,7 +56,12 @@ const GenericDetails = () => {
             </div>
 
             <div className="information w-[100%] bg-gray-100 shadow-md p-5">
-              <p className="uppercase">Side Effects of {genericData.name}</p>
+              <p className="uppercase">
+                Side Effects of{" "}
+                <span className="text-red-500 font-semibold">
+                  {genericData.name}
+                </span>{" "}
+              </p>
               <div className="bg-gray-300 h-[1px] w-[270px] mt-1"></div>
               <div className="text-justify">
                 {parse(`<p>${genericData?.sideEffects}</p>`)}
@@ -61,7 +70,10 @@ const GenericDetails = () => {
 
             <div className="information w-[100%] bg-gray-100 shadow-md p-5">
               <p className="uppercase">
-                Expert Advice for {genericData.name}
+                Expert Advice for{" "}
+                <span className="text-red-500 font-semibold">
+                  {genericData.name}
+                </span>{" "}
               </p>
               <div className="bg-gray-300 h-[1px] w-[270px] mt-1"></div>
 
