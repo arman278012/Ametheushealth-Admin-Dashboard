5cb58593cff0c9269cfe1e5c3394935b6880202d
arman278012 <aa278012@gmail.com>
2024-07-17 18:42:50 +0530

    instant-generic-updated

--- src/Components/InstantGeneric/InstantGeneric.js
+++ src/Components/InstantGeneric/InstantGeneric.js
@@ -67,60 +67,85 @@ const InstantGeneric = () => {
 
   return (
     <>
-      <div className="w-[75vw] mx-auto flex flex-col justify-center items-center py-5 gap-5 border-2">
+      <div className="w-[75vw] mx-auto flex flex-col justify-center items-center py-1 gap-5 border-2 mt-1">
         <p className="font-bold text-xl">Add generics</p>
-        <div className="">
-          <form onSubmit={handleSubmit}>
-            <div className="flex gap-4">
-              <input
-                type="text"
-                name="name"
-                value={formData.name}
-                onChange={handleChange}
-                placeholder="Enter data here..."
-                className="p-3 border rounded-xl h-[45px] w-[300px]"
-              />
-              <button
-                type="submit"
-                className="bg-green-500 hover:bg-green-600 flex justify-center items-center p-3 border rounded-xl h-[45px] text-white font-bold"
-              >
-                Add data
-              </button>
-            </div>
-          </form>
+        <div className="w-[100%] flex">
+          <div className="w-[50vw]">
+            <form onSubmit={handleSubmit}>
+              <div className="flex gap-4">
+                <input
+                  type="text"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  placeholder="Enter data here..."
+                  className="p-3 border rounded-xl h-[45px] w-[300px]"
+                />
+                <button
+                  type="submit"
+                  className="bg-green-500 hover:bg-green-600 flex justify-center items-center p-3 border rounded-xl h-[45px] text-white font-bold"
+                >
+                  Add data
+                </button>
+              </div>
+            </form>
 
-          <div className="flex justify-around mt-5 items-center">
-            {genericData.name && (
-              <div className="flex items-center gap-2 cursor-pointer">
-                <p 
-                className="cursor-pointer bg-blue-500 px-3 py-1 font-bold text-white"
-                onClick={() => handleCopy(genericData.name)}>
-                  {genericData.name}
-                </p>
-              </div>
-            )}
-            {genericData._id && (
-              <div className="flex items-center gap-2 cursor-pointer">
-                <p 
-                className="cursor-pointer bg-blue-500 px-3 py-1 font-bold text-white"
-                onClick={() => handleCopy(genericData._id)}>
-                  {genericData._id}
-                </p>
-              </div>
-            )}
+            <div className="flex gap-5 mt-5">
+              {genericData.name && (
+                <div className="flex items-center gap-2 cursor-pointer">
+                  <p
+                    className="cursor-pointer bg-blue-500 px-3 py-1 font-bold text-white"
+                    onClick={() => handleCopy(genericData.name)}
+                  >
+                    {genericData.name}
+                  </p>
+                </div>
+              )}
+              {genericData._id && (
+                <div className="flex items-center gap-2 cursor-pointer">
+                  <p
+                    className="cursor-pointer bg-blue-500 px-3 py-1 font-bold text-white"
+                    onClick={() => handleCopy(genericData._id)}
+                  >
+                    {genericData._id}
+                  </p>
+                </div>
+              )}
+              {(genericData.name || genericData._id) && (
+                <button
+                  onClick={handleDelete}
+                  className="cursor-pointer bg-red-500 px-2 font-bold text-white"
+                >
+                  <FaTrash className="" />
+                </button>
+              )}
+            </div>
           </div>
 
-          {(genericData.name || genericData._id) && (
-            <button
-              onClick={handleDelete}
-              className="mt-5 bg-red-500 hover:bg-red-600 flex justify-center items-center p-3 border rounded-xl h-[45px] text-white font-bold"
-            >
-              <FaTrash className="mr-2" />
-              Delete Both
-            </button>
-          )}
+          <div className="w-[50%]">
+            <form onSubmit={handleSubmit}>
+              <div className="flex gap-4">
+                <input
+                  type="text"
+                  name="name"
+                  //   value={formData.name}
+                  //   onChange={handleChange}
+                  placeholder="search data here..."
+                  className="p-3 border rounded-xl h-[45px] w-[300px]"
+                />
+                <button
+                  type="submit"
+                  className="bg-green-500 hover:bg-green-600 flex justify-center items-center p-3 border rounded-xl h-[45px] text-white font-bold"
+                >
+                  Search
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
+
+      <div></div>
     </>
   );
 };
