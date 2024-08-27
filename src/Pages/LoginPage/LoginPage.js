f8d0079d6b6fc9c99964ceacdd041ab9775ff833
arman278012 <aa278012@gmail.com>
2024-08-27 14:04:27 +0530

    bug fixed in add product category

--- src/Pages/LoginPage/LoginPage.js
+++ src/Pages/LoginPage/LoginPage.js
@@ -4,10 +4,6 @@ import Login from "../../Components/Login/Login";
 const LoginPage = () => {
   return (
     <div className="flex">
-      {/* <div>
-        <MySideBar />
-      </div> */}
-
       <div className="w-[100%]">
         <Login />
       </div>
---
+++ webpack.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // Other configurations...
+  stats: {
+    warnings: false,
+  },
+};
