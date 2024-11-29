ba6744f547faeb032b895c142c2ca5d956606bd7
Arman Ali <aa278012@gmail.com>
2024-11-29 11:27:40 +0530

    hostinger bug fixed

--- src/Components/GetAllDoctors/GetAllDoctors.js
+++ src/Components/GetAllDoctors/GetAllDoctors.js
@@ -392,9 +392,7 @@ const GetAllDoctors = () => {
         <div className="fixed inset-0 flex items-center justify-center z-50">
           <div className="absolute inset-0"></div>
           <div className="bg-white p-6 rounded-lg border-2 z-10">
-            <p className="text-lg mb-4">
-              Are you sure you want to delete this item?
-            </p>
+            <p className="text-lg mb-4">Do you want to delete this item?</p>
             <div className="flex justify-end">
               <button
                 onClick={() => {
