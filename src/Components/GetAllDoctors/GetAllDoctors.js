c660c4d3e86ed7efaf1efb29b29e031e920dc7bd
Arman Ali <aa278012@gmail.com>
2024-11-29 11:20:31 +0530

    hostinger bug fixed

--- src/Components/GetAllDoctors/GetAllDoctors.js
+++ src/Components/GetAllDoctors/GetAllDoctors.js
@@ -398,7 +398,6 @@ const GetAllDoctors = () => {
             <div className="flex justify-end">
               <button
                 onClick={() => {
-                  deleteDoctor(deleteId);
                   setDeleteAlert(false);
                 }}
                 className="bg-red-500 text-white px-4 py-2 rounded-md mr-2"
